6192fcfe43652b58b5b2aea9a929d152cba110d7
RAAVI BALA SIVA DURGA PRASAD <84697652+RBSDP@users.noreply.github.com>
2025-06-25 09:15:00 +0000

    updated register route in auth controller

--- fullstacksql/controller/auth.controller.js
+++ fullstacksql/controller/auth.controller.js
@@ -1,3 +1,35 @@
+import {PrismaClient} from "@prisma/client"
+import {bcrypt} from "bcryptjs"
+const prisma = new PrismaClient()
+
 export const registerUser = async (req, res) =>{
-    console.log("register user")
+    
+    const {email, password, name } = req.body;
+
+    if (!email || !password || !name){
+        console.log("data is missing")
+        return res.status(400).json({
+            success: false,
+            message : "All feild are required"
+        })
+    }
+
+
+
+
+    try {
+        
+        const existingUser = await prisma.user.findUnique({
+            where: {email}
+        })
+
+        if(!existingUser){
+            return res.status(400).json({
+            success: false,
+            message : "user already exists"
+        })}
+
+    } catch (error) {
+        
+    }
 }
